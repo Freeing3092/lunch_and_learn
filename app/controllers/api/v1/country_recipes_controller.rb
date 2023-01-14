deb2d28faaf9ebbcb81bf1fb167fb824e0b85ff9
Rich Kaht <adamselene1776@protonmail.com>
2023-01-14 16:20:29 -0700

    Feat/Test: get recipes for a random country if the country is not specified

--- app/controllers/api/v1/country_recipes_controller.rb
+++ app/controllers/api/v1/country_recipes_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::CountryRecipesController < ApplicationController
   def index
+    if params[:country].nil?
+      params[:country] = CountryFacade.random_country
+    end
     country_recipes = RecipesFacade.country_recipes(params[:country])
     
     render json: RecipesSerializer.new(country_recipes).serializable_hash.to_json
-    # require 'pry'; binding.pry
   end
 end
