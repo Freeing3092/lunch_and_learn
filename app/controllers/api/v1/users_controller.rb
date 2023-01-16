9f616ddfa41a78e78b923135c83b5e07742eb44b
Rich Kaht <adamselene1776@protonmail.com>
2023-01-15 21:52:45 -0700

    Fix: return 400 error if a user with the provided email already exists

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,12 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     user.api_key = SecureRandom.urlsafe_base64
-    user.save
 
-    render json: UserSerializer.new(user), status: 201
+    if user.save
+      render json: UserSerializer.new(user), status: 201
+    elsif User.find_by(email: user.email)
+      render json: {error: 'A user with this email has already been registered'}.to_json, status: 400
+    end
   end
 
   private
